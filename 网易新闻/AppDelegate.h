b3c6d64f9204964ed56f106a7d6c52dca7dc80ef
qianfeng <qianfeng@sunbei.local>
2014-11-18 20:02:12 +0800

    Initial Commit

---
+++ 网易新闻/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by qianfeng on 14-11-18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 网易新闻/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  网易新闻
+//
+//  Created by qianfeng on 14-11-18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
